b5e93ac9dbd1f45f93776586d244cb3df718b9fa
Seyoung Kim <62585390+Seyoung1220@users.noreply.github.com>
2022-05-24 01:02:59 +0900

    Update main.java

--- main.java
+++ main.java
@@ -15,11 +15,11 @@ public class main {
         System.out.print("학번을 입력하세요: ");
         num = sc.nextInt();
 
-        System.out.print("대학교를 입력하세요: ");
-        school = sc.next();
-
         System.out.print("학과를 입력하세요: ");
         major = sc.next();
+        
+        System.out.print("대학교를 입력하세요: ");
+        school = sc.next();
 
         System.out.println("\n<출력> \n이름: " + name + "\n학번: " + num + "\n대학교: " + school +"\n학과: " + major);
 
