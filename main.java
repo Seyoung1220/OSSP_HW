469190fc55830a23ba0b1b5e397f0499acfa1c40
Seyoung1220 <ihtmy12@gmail.com>
2022-05-24 00:54:32 +0900

    MODIFY JAVA_SCHOOL

--- main.java
+++ main.java
@@ -6,7 +6,7 @@ public class main {
         Scanner sc = new Scanner(System.in);
 
         int num;
-        String name;
+        String name, school;
 
         System.out.print("이름을 입력하세요: ");
         name = sc.next();
@@ -14,7 +14,10 @@ public class main {
         System.out.print("학번을 입력하세요: ");
         num = sc.nextInt();
 
-        System.out.println("\n<출력> \n이름: " + name + "\n학번: " + num);
+        System.out.print("대학교를 입력하세요: ");
+        school = sc.next();
+
+        System.out.println("\n<출력> \n이름: " + name + "\n학번: " + num + "\n대학교: " + school);
 
     }
 }
